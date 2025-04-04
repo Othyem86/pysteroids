2a37eec2fcdcd4e8b747ae1f0949fad97f10c8e5
Othyem86 <emil.foeger@gmail.com>
2025-04-04 22:34:12 +0200

    CHANGE: created player object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -7,11 +8,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 255))
+
+        screen.fill(SCREEN_BACKGROUND_COLOR)
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the game to 60 FPS
