51bd5dd587e61c18d50cd9ee46763a3fda88a07e
Othyem86 <emil.foeger@gmail.com>
2025-04-05 18:31:02 +0200

    CHANGE: added player movement

--- player.py
+++ player.py
@@ -7,12 +7,20 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
+    def move(self, dt: float) -> None:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def rotate(self, dt: float) -> None:
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt: pygame.Surface) -> None:
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
