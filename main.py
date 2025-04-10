924589ae6cb50fef189ec0192d6a36fa530e15d5
Othyem86 <emil.foeger@gmail.com>
2025-04-10 09:31:49 +0200

    CHANGE: added player to updateable and drawable sprite groups

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def main() -> None:
     clock = pygame.time.Clock()
     dt = 0
 
+    # Game object groups
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # Main game loop
@@ -17,15 +22,18 @@ def main() -> None:
                 return
 
         # Update
-        player.update(dt)
+        for obj in updateable:
+            obj.update(dt)
 
         # Draw
         screen.fill(GFX_SCREEN_BACKGROUND_COLOR)
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
 
         # limit the game to 60 FPS
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
